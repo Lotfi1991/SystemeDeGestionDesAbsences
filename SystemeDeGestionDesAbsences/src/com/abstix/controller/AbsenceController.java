32dea4402587ed221b6389fb1818eee01c913a37
Abdellatif AZZERRIFI LAAMRANI <abdellatif.laamrani@gmail.com>
2015-11-23 17:09:33 +0100

    Version 1.1

---
+++ SystemeDeGestionDesAbsences/src/com/abstix/controller/AbsenceController.java
@@ -0,0 +1,14 @@
+package com.abstix.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class AbsenceController {
+
+	public AbsenceController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
